24a815b7cbd524cdf7d7d616163aec5be33ba31a
shimhyunwoong <shimhyunwoong@htbeyond.com>
2022-07-17 16:45:09 +0900

    fix: pageRequestDto add

---
+++ src/main/kotlin/com/example/test/user/dto/PageRequestDto.kt
@@ -0,0 +1,8 @@
+package com.example.test.user.dto
+
+data class PageRequestDto(
+    val page: Int, //페이지 번호
+    val size: Int, //표시할 정보의 수
+    val sortBy: String, //정렬 기준
+    val isAsc: Boolean //오름차순, 내림차순
+)
